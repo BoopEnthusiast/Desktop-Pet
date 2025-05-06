97137066ae1bbe44da560bb8d607be6783e0a5e2
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-05-06 16:33:06 +1200

    Remove debug window

--- main.gd
+++ main.gd
@@ -3,29 +3,25 @@ extends Node2D
 
 const SPEED = 400
 
-var previous_monitor: int
-
 @onready var icon: Sprite2D = $Icon
-@onready var debug_window: Window = $Window
 
 
 func _ready() -> void:
 	get_window().size = icon.texture.get_size()
 
 
-
 func _physics_process(delta: float) -> void:
 	var window = get_window()
-	var moving_in_rect: Rect2 = Rect2(window.position.x, window.position.y, window.size.x, window.size.y)
+	var moving_in_rect: Rect2 = Rect2(window.position.x, window.position.y, window.size.x, window.size.y) # The rect of where the pet is moving to
 	
 	# Get the moving direction according to input
-	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+	var direction: Vector2 = Input.get_vector("left", "right", "up", "down").normalized()
 	moving_in_rect.position += direction * SPEED * delta
 	
-	debug_window.position = Vector2i(moving_in_rect.position + moving_in_rect.size / 2)
+	# Get the middle of where is trying to be moved
+	var moving_to_position: Vector2 = moving_in_rect.position + moving_in_rect.size / 2 
 	
 	# Make sure the window is inside the usable rect of the screen
-	var moving_to_position: Vector2 = moving_in_rect.position + moving_in_rect.size / 2
 	var outside_of_all_areas: bool = true
 	for i in range(DisplayServer.get_screen_count()):
 		var screen_usable_rect = DisplayServer.screen_get_usable_rect(i)
@@ -36,7 +32,7 @@ func _physics_process(delta: float) -> void:
 				and moving_to_position.y > screen_usable_rect.position.y
 		):
 			outside_of_all_areas = false
-			previous_monitor = i
 	
+	# Move the pet if it's not moving outside of the screen
 	if not outside_of_all_areas:
 		window.position += Vector2i(direction * SPEED * delta)
