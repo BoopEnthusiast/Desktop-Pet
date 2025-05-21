db97fa8c1294355fa0c126f3ea3b78b7ebc7dbf6
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-05-21 16:40:50 +1200

    start of modules screen

--- menu.gd
+++ menu.gd
@@ -3,6 +3,7 @@ extends PanelContainer
 
 
 @onready var settings: Settings = $VSplitContainer/Settings
+@onready var modules: HSplitContainer = $VSplitContainer/Modules
 
 
 func _enter_tree() -> void:
@@ -14,4 +15,9 @@ func _on_close_pressed():
 
 
 func _on_settings_pressed() -> void:
-	settings.visible = not settings.visible
+	if settings.visible:
+		settings.visible = false
+		modules.visible = true
+	else:
+		settings.visible = true
+		modules.visible = false
---
+++ modules/modules.gd
@@ -0,0 +1,2 @@
+class_name Modules
+extends HSplitContainer
