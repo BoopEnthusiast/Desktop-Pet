563de2c58bffb6a4ba08acb188b30f7afcd0edbd
Boop <onpotis@gmail.com>
2025-12-02 08:51:32 +1300

    New flower scene/script

---
+++ taskbar_flowers/flower_old.gd
@@ -0,0 +1,35 @@
+class_name FlowerOld
+extends Area2D
+
+
+signal deleted_flower(flower: FlowerOld)
+signal new_growth_height(height: float)
+
+var start_position: Vector2
+
+@onready var polygon: PackedVector2Array:
+	get():
+		return stem.polygon
+
+@onready var stem: FlowerStem = $Stem
+
+
+func _ready() -> void:
+	stem.next_height = start_position.y
+	stem.path.curve.set_point_position(0, start_position)
+
+
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	print("Flower interacted with")
+	if event.is_action_pressed(&"click"):
+		print("Flower clicked")
+		deleted_flower.emit(self)
+		queue_free()
+
+
+func _on_stem_new_growth_height() -> void:
+	new_growth_height.emit(stem.next_height)
+
+
+func _on_mouse_entered() -> void:
+	print("Mouse entered flower")
