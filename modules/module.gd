7519136092cab076df2111fb2967f0bbb53c4551
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2026-01-28 16:06:32 +1300

    Start of lil guys

--- modules/module.gd
+++ modules/module.gd
@@ -1,9 +1,52 @@
+@abstract
 class_name Module
 extends Window
 
 
+## The method used for scaling
+enum ScalingMode {
+	## Doesn't do any scaling, it's up to you
+	NO_SCALING,
+	## Covers all screens as a rectangle. This includes areas not available on the screens
+	ALL_SCREENS,
+	## Makes the window cover the full [member screen] excluding the taskbar
+	USABLE,
+	## Stick to the bottom of the [member screen]
+	BOTTOM,
+	## Stick to the top of the [member screen]
+	TOP,
+	## Stick to the left of the [member screen]
+	LEFT,
+	## Stick to the right of the [member screen]
+	RIGHT,
+}
+
+## The name of the module in the system
 @export var module_title: String = "New Module Title"
+## The list of settings you want to appear on the settings page in the main screen
 @export var module_settings: Array[ModuleSetting]
+@export_group("Scaling", "scaling_")
+## The way it changes the size of the window automatically. This assumes [member Window.unresizable] is true
+@export var scaling_mode: ScalingMode = ScalingMode.NO_SCALING
+## Which screen to use [member scaling_mode] if the mode uses it.[br]
+## [br]
+## The constants [constant DisplayServer.SCREEN_OF_MAIN_WINDOW],
+## [constant DisplayServer.SCREEN_OF_MAIN_WINDOW],
+## [constant DisplayServer.SCREEN_PRIMARY], 
+## [constant DisplayServer.SCREEN_WITH_MOUSE_FOCUS],
+## or [constant DisplayServer.SCREEN_WITH_KEYBOARD_FOCUS]
+## are also available to be used.
+@export_range(-4, 3, 1.0, "or_greater") var scaling_screen: int = -1
+
+
+func _init() -> void:
+	close_requested.connect(update_window)
+	focus_entered.connect(update_window)
+	focus_exited.connect(update_window)
+	mouse_entered.connect(update_window)
+	mouse_exited.connect(update_window)
+	ready.connect(update_window)
+	size_changed.connect(update_window)
 
 
 func _ready() -> void:
@@ -18,3 +61,28 @@ func _input(event: InputEvent) -> void:
 func _on_updated_video_config() -> void:
 	msaa_2d = Config.video_config_file.get_value("Video", "msaa", msaa_2d)
 	screen_space_aa = Config.video_config_file.get_value("Video", "ssaa", screen_space_aa)
+
+
+## Called during
+## [signal Window.close_requested],
+## [signal Window.focus_entered],
+## [signal Window.focus_exited],
+## [signal Window.mouse_entered],
+## [signal Window.mouse_exited],
+## and [signal Node.ready].[br]
+## It updates the size of the window when it could be needed.
+func update_window() -> void:
+	# Update the window
+	match scaling_mode:
+		ScalingMode.ALL_SCREENS:
+			pass
+		ScalingMode.USABLE:
+			size = DisplayServer.screen_get_usable_rect(scaling_screen).size
+		ScalingMode.BOTTOM:
+			pass
+		ScalingMode.TOP:
+			pass
+		ScalingMode.LEFT:
+			pass
+		ScalingMode.RIGHT:
+			pass
--- taskbar_flowers/taskbar_flowers.gd
+++ taskbar_flowers/taskbar_flowers.gd
@@ -5,6 +5,8 @@ extends Module
 var flowers: Array[Flower]
 var tallest_flower: Flower
 
+var previous_size: Vector2i
+
 var merge_polygon: PackedVector2Array
 var height: float
 
@@ -18,16 +20,15 @@ func _on_new_flower_made(new_flower: Flower) -> void:
 	flowers.append(new_flower)
 
 
-func _update_window() -> void:
-	# Update the window
-	var usable_rect: Rect2i = DisplayServer.screen_get_usable_rect(DisplayServer.get_primary_screen())
+func update_window() -> void:
+	super()
 	
-	if usable_rect.size != size:
+	if previous_size != size:
 		# Move flowers
 		for flower: Flower in flowers:
 			flower.position.y = size.y
 	
-	size = usable_rect.size
+	previous_size = size
 	
 	if not is_node_ready():
 		await ready
@@ -38,16 +39,14 @@ func _update_window() -> void:
 	merge_polygon = [
 		Vector2(0.0, 0.0),
 		Vector2(0.0, -1.0),
-		Vector2(usable_rect.size.x, -1.0),
-		Vector2(usable_rect.size.x, 0.0),
+		Vector2(size.x, -1.0),
+		Vector2(size.x, 0.0),
 	]
 	merge_polygon_node.polygon = merge_polygon
 	height = size.y
 	
-	mouse_passthrough = true
-	mouse_passthrough = false
-	
 	# TODO: remove all newly out-of-bounds flowers and make flowers not want to go out of bounds
+	# TODO instead: move flowers according to how large the screen is, so if the screen gets smaller it squishes them in
 
 
 func _on_close_requested() -> void:
